06e3429584ddcd905c317889b0856ff7a04f1cd4
Kevin Nguyen <me@kndwin.dev>
2021-03-22 23:02:53 +1100

    ➕ added links to /jobs

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,12 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtjs
-      </h1>
-      <h2 class="subtitle">
-        My astonishing Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+		<h1>Hello world</h1>
+		<NuxtLink to="/jobs">Jobs</NuxtLink>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
@@ -43,30 +14,9 @@ export default {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
+	flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
---
+++ pages/jobs.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+		<h1>Jobs</h1>
+		<h2>
+		</h2>
+		<NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
