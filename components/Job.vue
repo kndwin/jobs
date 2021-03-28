c288ba18f4352d10ee346ac375286a2134b0aea7
Kevin Nguyen <me@kndwin.dev>
2021-03-29 03:12:28 +1100

    ➕ added description to job cards

--- components/Job.vue
+++ components/Job.vue
@@ -1,29 +1,55 @@
 <template>
 	<div class="job">
-		<h2 class="title">
-			<a v-bind:href=job.link>
-				{{ job.title }}
-			</a>
-		</h2>
-		<div class="companyAndTimePostedWrapper">
+			<div class="flexRow">
+				<span class="source">
+					{{ job.source }}
+				</span>
+		<a v-bind:href=job.link>
+				<h2 class="title">
+						{{ job.title }}
+				</h2>
+		</a>
+			</div>
+		<br>
+		<div class="flexRow">
 			<img class="icon"
 				alt="Icon of building"
 				src="https://img.icons8.com/ios/50/000000/building.png"/>
 			<p class="company">
 				{{ job.company }}
 			</p>
-			<p class="timePosted">
+			<p  class="timePosted">
 				{{ job.timePosted }}
 			</p>
 		</div>
-		<p class="source">{{ job.source }}</p>
+		<br>
+		<a  class="show"
+			v-on:click="show = !show">
+			<span v-if="!show">
+				Show More
+			</span>
+			<span v-else>
+				Hide
+			</span>
+		</a>
+		<div class="descriptionHTML" 
+				v-html=job.descriptionHTML 
+			 v-if="show"
+			>
+			Hover
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  props: ['job', 'id']	
+  props: ['job', 'id'], 
+	data() {
+		return {
+			show: false
+		}
+	}
 })
 </script>
 
@@ -35,7 +61,9 @@ export default Vue.extend({
   margin: 0.5rem;
 }
 .source {
-  padding: 0.5rem;
+  padding:  .45em .3em .3em .45em;
+	margin: 0 1rem 0 0;
+	font-size: 12pt; 
   font-weight: bold;
   background-color: #efefef;
   width: fit-content;
@@ -50,7 +78,7 @@ export default Vue.extend({
 	color: #666;
 }
 
-.companyAndTimePostedWrapper {
+.flexRow {
 	display: flex;
 	align-items: baseline;
 	overflow: hidden; 
@@ -62,22 +90,33 @@ export default Vue.extend({
 }
 
 .title {
-  a {
-    text-decoration: none;
-    font-size: 14pt;
-    color: black;
-  }
+	display: flex; 
+	align-items: flex-start; 
+}
+
+h2 {
+	padding: 0.3em 0 0 0;
+	margin: 0 0 0.3em 0;
+}
+
+a {
+	cursor: pointer; 
+	text-decoration: none;
+	color: black;
 }
 
 .icon {
 	height: 1em;
-	margin: 0 0.2rem 0 0; 
+	margin: 0 0.4rem 0 0; 
 }
 
-@media (width: 700px) {
-	.companyAndTimePostedWrapper {
-		display: block;
-	}
+.descriptionHTML {
+	margin: 1em 0;
+	padding: 1em; 
+	border: 1px solid black; 
+}
+
+.show {
 }
 
 </style>
